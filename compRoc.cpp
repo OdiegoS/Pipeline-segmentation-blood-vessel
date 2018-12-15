851801676d48afa90e269080dac5ed0eac8d44f4
Diego Oliveira <diego.so.pgm@gmail.com>
2018-12-15 11:22:42 -0300

    Uploading all files

---
+++ compRoc.cpp
@@ -0,0 +1,298 @@
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/highgui/highgui_c.h>
+
+#include <opencv2/imgproc.hpp>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct Roc{
+	// igs[0] = verdadeiro negativo (TN). igs[1] = verdadeiro positivo (TP)	
+	float igs[2];
+	// dif[0] = falso negativo (FN). dif[1] = falso positivo (FP)
+	float dif[2];
+}curvaR;
+
+typedef struct estrutRoc{
+	curvaR *cR = NULL;
+}eR;
+
+
+void taxa(float igs[2], float dif[2], float val[]){
+	val[0] = ( igs[1]/(igs[1]+dif[0]) );
+	val[1] = ( igs[0]/(igs[0]+dif[1]) );
+	val[2] = ( dif[1]/(igs[0]+dif[1]) );
+	val[3] = ( dif[0]/(igs[1]+dif[0]) );
+	val[4] = ( igs[1]/(igs[1]+dif[1]) );
+	val[5] = ( igs[0]/(igs[0]+dif[0]) );
+	val[6] = ( (igs[1]+igs[0])/(igs[0]+igs[1]+dif[0]+dif[1]) );
+	
+	//printf("Val 0: %f\nVal 1: %f\nVal 2: %f\nVal 3: %f\n", val[0], val[1], val[2], val[3]);
+	
+}
+// 0 preto, -1 n-preto
+void curvasRoc(IplImage *imgMet,IplImage *imgDb, int tam, curvaR *cR){
+	int i;
+
+	for(i=0;i<2;i++){
+		cR->igs[i] = 0;
+		cR->dif[i] = 0;
+	}
+
+	//Binarizando as imagens
+	IplImage *tmp = cvCreateImage( cvGetSize(imgMet), imgMet->depth, imgMet->nChannels);
+	//opcional
+	cvThreshold(imgMet, tmp, 0, 255, CV_THRESH_BINARY);	
+	cvCopy(tmp,imgMet);
+
+	cvThreshold(imgDb, tmp, 3, 255, CV_THRESH_BINARY);
+	cvCopy(tmp,imgDb);
+
+
+	//Calculando os valores de TN, TP, FN e FP
+	for(i=0;i<tam;i++){
+	    // Se forem iguais: podem ser TN ou TP
+	    if(imgMet->imageData[i] == imgDb->imageData[i]){
+		// Se forem iguais com valor 0, então é TN
+		if(imgMet->imageData[i] == 0){
+			cR->igs[0]++;
+		// Se forem iguais com valor acima de 0, então é TP
+		}else{
+			cR->igs[1]++;
+		}
+	    // Se forem diferentes: podem ser FN ou FP
+	    }else{
+		// Se forem diferentes e imagem obtida for 0, então é um FN
+		if(imgMet->imageData[i] == 0){
+			cR->dif[0]++;
+		// Se forem diferentes e imagem obtida for acima de 0, então é um FP
+		}else{
+			cR->dif[1]++;
+		}
+	    }
+ 	}
+
+//	printf("Tam: %d\nVN: %f  VP: %f\nFN: %f   FP: %f\n", tam, cR->igs[0], cR->igs[1], cR->dif[0], cR->dif[1]);
+}
+
+void salvarFile(FILE *saida, int numImg, int diretorio, eR *comp){
+	int i,j,k;
+	float val[7] = { 0, 0, 0, 0, 0, 0, 0};
+
+	for(i=1;i<=diretorio;i++){
+		if(i==1){
+			fprintf(saida, "        |                                Referencia 1                                |");
+		}else{
+			fprintf(saida, "|                                Referencia %d                                |", i);
+		}
+	}
+	fprintf(saida,"\n Imagem |");
+  	for(i=1;i<=diretorio;i++){
+		if(i!=diretorio){
+			fprintf(saida, "  SN(TPR) |  SP(TNR) |   FPR    |   FNR    |   PPV    |   NPV    |   ACC    ||");
+		}else{
+			fprintf(saida, "  SN(TPR) |  SP(TNR) |   FPR    |   FNR    |   PPV    |   NPV    |   ACC    |");
+		}
+  	}
+
+	for(i=0;i<numImg;i++){
+		if(i<9){
+			fprintf(saida, "\n   0%d   |", i+1);
+		}else{
+			fprintf(saida, "\n   %d   |", i+1);
+		}
+
+		for(j=0;j<diretorio;j++){
+			taxa(comp[j].cR[i].igs, comp[j].cR[i].dif, val);
+			k = 0;			
+			while(k != 7){
+				if(val[k] < 0.100){
+					fprintf(saida,"  0%.2f%c  |", val[k]*100, 37);
+				}else{
+					fprintf(saida,"  %.2f%c  |", val[k]*100, 37);
+				}
+				k++;
+			}
+			if(j != (diretorio-1) ){
+				fprintf(saida, "|");
+			}
+		}	
+	}
+
+
+	for(i=0;i<diretorio;i++){
+		if(i==0){
+			fprintf(saida,"\n-------------------------------------------------------------------------------------|");
+		}else{
+			fprintf(saida,"|----------------------------------------------------------------------------|");
+		}
+	}
+
+	fprintf(saida,"\n Tot(%c) |", 37);
+
+	curvaR *suporte = (curvaR*)malloc( diretorio * sizeof(curvaR) );
+	for(i=0;i<diretorio;i++){
+		suporte[i].igs[0] = 0;
+		suporte[i].igs[1] = 0;
+		suporte[i].dif[0] = 0;
+		suporte[i].dif[1] = 0;
+	}
+	
+	for(i=0;i<diretorio;i++){
+		for(j=1;j<numImg;j++){ 
+			suporte[i].igs[0] = suporte[i].igs[0] + comp[i].cR[j].igs[0];
+			suporte[i].igs[1] = suporte[i].igs[1] + comp[i].cR[j].igs[1];
+			suporte[i].dif[0] = suporte[i].dif[0] + comp[i].cR[j].dif[0];
+			suporte[i].dif[1] = suporte[i].dif[1] + comp[i].cR[j].dif[1];
+		}
+		taxa(suporte[i].igs, suporte[i].dif, val);
+		k = 0;			
+		while(k != 7){
+			if(val[k] < 0.100){
+				fprintf(saida,"  0%.2f%c  |", val[k]*100, 37);
+			}else{
+				fprintf(saida,"  %.2f%c  |", val[k]*100, 37);
+			}
+			k++;
+		}
+		if(i != (diretorio-1) ){
+			fprintf(saida, "|");
+		}
+	}
+	for(i=1;i<diretorio;i++){
+		suporte[0].igs[0] = suporte[0].igs[0] + suporte[i].igs[0];
+		suporte[0].igs[1] = suporte[0].igs[1] + suporte[i].igs[1];
+		suporte[0].dif[0] = suporte[0].dif[0] + suporte[i].dif[0];
+		suporte[0].dif[1] = suporte[0].dif[1] + suporte[i].dif[1];
+	}
+	taxa(suporte[0].igs, suporte[0].dif, val);
+	fprintf(saida, "\n\n\n\n           |  SN(TPR) |  SP(TNR) |   FPR    |   FNR    |   PPV    |   NPV    |   ACC    |\n");
+	fprintf(saida," Geral(%c)  |", 37);
+	k=0;	
+	while(k != 7){
+		if(val[k] < 0.100){
+			fprintf(saida,"  0%.2f%c  |", val[k]*100, 37);
+		}else{
+			fprintf(saida,"  %.2f%c  |", val[k]*100, 37);
+		}
+		k++;
+	}
+
+	fprintf(saida, "\n\n\n\n");
+	for(i=1;i<=diretorio;i++){
+		if(i==1){
+			fprintf(saida, "                                   Referencia 1                        |");
+		}else{
+			fprintf(saida, "|                         Referencia %d                         |", i);
+		}
+	}
+	fprintf(saida,"\n Imagem |");
+  	for(i=1;i<=diretorio;i++){
+		if(i!=diretorio){
+			fprintf(saida, "   TP   |   TN   |   FP   |   FN   |  TP+FP |  TP+FN | Total  ||");
+		}else{
+			fprintf(saida, "   TP   |   TN   |   FP   |   FN   |  TP+FP |  TP+FN | Total  |");
+		}
+  	}
+
+
+	for(i=0;i<numImg;i++){
+		if(i<9){
+			fprintf(saida, "\n   0%d   |", i+1);
+		}else{
+			fprintf(saida, "\n   %d   |", i+1);
+		}
+
+		for(j=0;j<diretorio;j++){
+		  fprintf(saida," %6.f | %6.f |", comp[j].cR[i].igs[1], comp[j].cR[i].igs[0]);
+		  fprintf(saida," %6.f | %6.f |", comp[j].cR[i].dif[1], comp[j].cR[i].dif[0]);
+		  fprintf(saida," %6.f |", comp[j].cR[i].igs[1] + comp[j].cR[i].dif[1]);
+		  fprintf(saida," %6.f |", comp[j].cR[i].igs[1] + comp[j].cR[i].dif[0]);
+		  fprintf(saida," %6.f |", comp[j].cR[i].igs[1] + comp[j].cR[i].igs[0] + comp[j].cR[i].dif[0] + comp[j].cR[i].dif[1]);
+		  
+		  if(j != (diretorio-1) ){
+		  	fprintf(saida, "|");
+		  }
+		}	
+	}
+	fprintf(saida,"\n\n\n");
+}
+
+void processos(FILE *entrada, FILE *saida){
+  int numImg, diretorios,i,tam,j;
+  char diret[255];
+  IplImage *imgDb = NULL, *imgMet = NULL;
+  eR *comp = NULL;
+
+  fscanf(entrada,"%d", &diretorios);
+  fscanf(entrada,"%d", &numImg);
+
+  comp = (eR*)malloc(diretorios*sizeof(eR));
+  for(i=0;i<diretorios;i++){
+	comp[i].cR = (curvaR*)malloc(numImg*sizeof(curvaR));
+  }
+
+  //Iniciando o processo de comparacao
+  for(i=0;i<numImg;i++){
+
+    fscanf(entrada,"%s",diret);
+    imgMet = cvLoadImage(diret, CV_LOAD_IMAGE_GRAYSCALE);
+
+    if( imgMet == NULL){
+      printf("*** [Erro] Imagem gerada nao encontrada\n");
+      exit(1);
+    }
+
+    if( i == 0 ){
+      tam = imgMet->height * imgMet->width;
+    }
+
+    for(j=0;j<diretorios;j++){
+      fscanf(entrada,"%s",diret);
+      imgDb = cvLoadImage(diret, CV_LOAD_IMAGE_GRAYSCALE);
+
+      if( imgDb == NULL){
+        printf("*** [Erro] Imagem %d da referencia %d nao encontrada\n", i+1,j+1);
+        exit(1);
+      }
+
+      curvasRoc(imgMet, imgDb, tam, &comp[j].cR[i]);
+      cvReleaseImage(&imgDb);
+    }
+
+    cvReleaseImage(&imgMet);
+  }
+
+  salvarFile(saida,numImg,diretorios,comp);
+
+  for(i=0;i<diretorios;i++){
+	  free(comp[i].cR);
+	  comp[i].cR = NULL;
+  }
+  free(comp);
+  comp = NULL;
+
+}
+
+int main (int argc, char** argv){
+
+  FILE *entrada = fopen(argv[1],"r");
+  FILE *saida = fopen("Arquivos/ResultRoc01-40.txt","w");
+
+  if( entrada == NULL ){
+      printf("*** [Erro] Nao foi possivel abrir o arquivo de entrada\n");
+      exit(1);
+  }
+
+  if( saida == NULL ){
+      printf("*** [Erro] Nao foi possivel abrir o arquivo de saida\n");
+      exit(1);
+  }
+
+  processos(entrada, saida);
+
+  fflush(saida);
+  fclose(entrada);
+  fclose(saida);
+  return 0;
+}
---
+++ metodoFuzzy.cpp
@@ -0,0 +1,475 @@
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui_c.h>
+#define __OPENCL__
+#include <visiongl.h>
+
+int jan1 = 3;
+int jan2 = 3;
+int val1 = 0;
+int val2 = 0;
+int tsy = 4.0;
+int b = 1.0;
+
+void vglMin(VglImage *input_1, VglImage *input_2, VglImage *output){
+
+  vglCheckContext(output, VGL_RAM_CONTEXT);
+  vglCheckContext(output, VGL_RAM_CONTEXT);
+
+  cvMin(input_1->ipl, input_2->ipl, output->ipl );
+
+  vglSetContext(output, VGL_RAM_CONTEXT);
+  
+}
+
+void geodesicDilation(VglImage *mask, VglImage *marker, VglImage *temp, float element[],int x,int y, int type){
+
+  VglImage *imagemDilat = vglCreateImage(mask);
+
+  if(type > 0){
+	vglClFuzzyDilate(marker, imagemDilat,element,x,y,type);
+  }else{
+  	vglClDilate(marker,imagemDilat,element,x,y);
+  }
+  vglClDownload(imagemDilat);
+
+  vglMin(mask,imagemDilat,temp);
+
+  vglReleaseImage(&imagemDilat);
+}
+
+void reconstructionByDilation(VglImage *imagem, VglImage *marker, VglImage *temp, float element[],int x,int y,int type){
+
+  geodesicDilation(imagem, marker, temp, element,x,y, type);
+
+   int k=0, tam = imagem->shape[0]*imagem->shape[1];
+   for(int i = 0; i < tam; i++){
+     if( (marker->ipl->imageData[i] - temp->ipl->imageData[i]) != 0){
+      k = 1;
+      break;
+    }
+   }
+   if(k == 1){
+     reconstructionByDilation(imagem, temp, marker, element,x,y, type);
+   }
+
+}
+
+void openingByReconstruction(VglImage* src, VglImage* dst,VglImage *bufferEroded,VglImage *bufferTmp, float element[],int x,int y, int iterations, int type){
+
+  int i;
+  
+  if(iterations > 0){
+
+    if(type > 0){
+    	vglClFuzzyErode(src, bufferTmp,element,x,y,type);
+    }else{
+    	vglClErode(src,bufferTmp,element,x,y);
+    }  
+ 
+    for(int i = 1; i < iterations; i++){
+      if (i % 2 == 0){
+	if(type > 0){
+		vglClFuzzyErode(bufferEroded, bufferTmp,element,x,y,type);
+	}else{
+		vglClErode(bufferEroded, bufferTmp,element,x,y);
+	}
+      }else{
+	if(type > 0){
+		vglClFuzzyErode(bufferTmp, bufferEroded,element,x,y,type);
+	}else{
+		vglClErode(bufferTmp,bufferEroded,element,x,y);
+	}
+      }
+    }
+    if (iterations % 2 == 1){
+       vglClCopy(bufferTmp,bufferEroded);
+    }
+   }
+  vglClDownload(bufferEroded);
+
+  reconstructionByDilation(src, bufferEroded, bufferTmp, element,x,y, type);
+  
+  vglClCopy(bufferTmp,dst);
+  vglClDownload(dst);
+
+}
+
+
+void closingImage(VglImage *src, VglImage *dst,float element[],int x, int y, int iterations, int type){
+
+  int i;
+  VglImage *tmp = vglCreateImage(src);
+
+  if(iterations > 0){
+
+    if(type > 0){
+    	vglClFuzzyDilate(src,tmp,element,x,y,type);
+    }else{
+        vglClDilate(src,tmp,element,x,y); 
+    }
+    for(int i = 1; i < iterations; i++){
+      if (i % 2 == 0){
+	if(type > 0){
+		vglClFuzzyDilate(dst,tmp,element,x,y,type);
+	}else{
+		vglClDilate(dst,tmp,element,x,y);
+	}
+      }else{
+	if(type > 0){
+		vglClFuzzyDilate(tmp, dst,element,x,y,type);
+	}else{
+		vglClDilate(tmp,dst,element,x,y);
+	}
+      }
+    }
+    if (iterations % 2 == 1){
+      vglClCopy(tmp,dst);
+    }
+
+    if(type > 0){
+    	vglClFuzzyErode(dst,tmp,element,x,y,type);
+    }else{
+    	vglClErode(dst,tmp,element,x,y);  
+    }
+
+    for(int i = 1; i < iterations; i++){
+      if (i % 2 == 0){
+	if(type > 0){
+		vglClFuzzyErode(dst,tmp,element,x,y,type);
+	}else{
+		vglClErode(dst,tmp,element,x,y);
+	}
+      }else{
+	if(type > 0){
+		vglClFuzzyErode(tmp, dst,element,x,y,type);
+	}else{
+		vglClErode(tmp,dst,element,x,y);
+	}
+      }
+    }
+    if (iterations % 2 == 1){
+      vglClCopy(tmp,dst);
+      }
+  }
+  vglClDownload(dst);
+
+  vglReleaseImage(&tmp);
+
+}
+
+void blackHat (VglImage *src, VglImage *dst,float element[], int x, int y, int interations, int type){
+
+  VglImage *tmp = vglCreateImage(src);
+  VglImage *tmp2 = vglCreateImage(src);
+	
+  closingImage(src, tmp, element, x, y, interations, type);
+	
+  vglClSub(tmp, src, tmp2);
+  vglClCopy(tmp2, dst);
+  vglClDownload(dst);
+
+  vglReleaseImage(&tmp);
+  vglReleaseImage(&tmp2);
+
+}
+
+void gaussianBlurImage (IplImage *src, IplImage *dst, int Kwidth, int Kheight, int x, int y = 0){
+
+  using namespace cv;
+
+  Mat temp = cvarrToMat(src);
+  GaussianBlur(temp, temp, Size(Kwidth,Kheight),x,y);
+  *dst = temp;
+}
+
+float* gaussianFunction2D(int sizeX, int sizeY, float sigma){
+	int i, centro, limX, limY, x, y;
+	limX = (sizeX - 1) / 2;
+	limY = (sizeY - 1) / 2;
+        sigma = limX / sigma;
+	float t = 2*( pow(sigma,2) );
+	float m = 1 / (M_PI*t);
+	float fator, *element = NULL;
+	
+	if ( (sizeX % 2 == 0) || (sizeY % 2 == 0) ){
+		printf("[Error] Valor de X e Y nao podem ser par\n");
+		exit(1);
+	}
+	
+	if (sigma <= 0){
+		printf("[Error] Valor de sigma deve ser positivo nao nulo\n");
+		exit(1);
+	}
+	
+
+	element = (float*)malloc( (sizeX*sizeY)*sizeof(float) );
+	
+	centro = ( (sizeX*sizeY) - 1) / 2;
+	
+	//Calculando o valor para o centro e calculando o fator de normalização
+	element[centro] = m * ( pow(M_E, 0) );
+	fator = 1 / element[centro];
+	element[centro] = 1;
+	
+	//Calculando os valores das demais coordenadas utilizando no final o fator de normalização
+	x = -limX;
+	y = -limY;
+	
+	for(i=0; i<(sizeX*sizeY); i++){
+		
+		 if(i != centro ){
+		 	
+		 	float n = ( -( pow(x,2) + pow(y,2) ) ) / t;
+		 	element[i] = (m * ( pow(M_E, n)) )*fator;
+		 	
+		 }
+		 y++;
+		 
+		 if( y == (limY+1) ){
+		 	y = - limY;
+		 	x++;
+		 }
+	}
+	
+	return element;
+};
+
+void aplicarMask(IplImage *src, IplImage *mask, IplImage *dst){
+  IplImage *tmp = cvCloneImage(mask);
+
+  cvMin(src,mask,tmp);
+  cvCopy(tmp,dst);
+
+  cvReleaseImage(&tmp);
+}
+
+void metodo(FILE *entrada,int nImg,int type,int tam, int bh, float ts){
+  int i;
+  char diretorio[200];
+
+
+  vglClInit();
+  vglInit();
+
+  for(i = 0; i<nImg;i++){
+    if(i<9){
+    	printf("Image: 0%d\n", i+1);
+    }else{
+    	printf("Image: %d\n",i+1);
+    }
+  
+    fscanf(entrada,"%s", diretorio);
+/*
+    VglImage *vImage = vglLoadImage(diretorio);
+    float *element = gaussianFunction2D( tam, tam, ( (tam - 1) / 2) * (1.0/(5.0*3.0)) );
+//      float *element = gaussianFunction2D( tam, tam, ( (tam - 1) / 2) * (1.0/(5.0*tam)) );
+      IplImage *imagem = cvCreateImage(cvGetSize(vImage->ipl),vImage->ipl->depth, 3);
+      IplImage *temp = cvCreateImage(cvGetSize(vImage->ipl),vImage->ipl->depth, 1);
+      cvSplit(vImage->ipl, NULL, temp, NULL, NULL);
+
+      IplImage *mask = cvCreateImage(cvGetSize(temp),temp->depth, temp->nChannels);
+      cvThreshold(temp,mask, 20, 255, CV_THRESH_BINARY);
+      aplicarMask(temp,mask,temp);
+
+      gaussianBlurImage(temp,temp,5,5,2,1);
+      cvMerge(temp,temp,temp,NULL, imagem);
+   //   VglImage *vImage = vglCopyCreateImage( imagem );
+      vImage = vglCopyCreateImage( imagem );
+      vglImage3To4Channels(vImage);
+
+      VglImage *tmp = vglCreateImage(vImage);
+
+      blackHat(vImage, vImage, element, tam, tam, bh, type);
+
+      vglImage4To3Channels(vImage);
+
+      IplImage *mask2 = cvCreateImage(cvGetSize(imagem),imagem->depth, imagem->nChannels);
+      cvMerge(mask,mask,mask,NULL,mask2);
+      cvCopy(vImage->ipl, imagem);
+      cvErode(mask2,mask2, NULL, 10);
+      aplicarMask(imagem,mask2,imagem);
+      vImage = vglCopyCreateImage( imagem );
+      cvReleaseImage(&imagem);
+
+      vglImage3To4Channels(vImage);
+
+      vglClThreshold(vImage, tmp, ts/255.0 , 1.0);
+      vglClCopy(tmp,vImage);
+      vglClDownload(vImage);
+
+      VglImage *buffer1 = vglCreateImage(vImage);
+      VglImage *buffer2 = vglCreateImage(vImage);
+
+      openingByReconstruction(vImage,vImage,buffer1,buffer2,element,tam,tam,1, type);
+      vglReleaseImage(&buffer2);
+
+      vglClThreshold(vImage, buffer1, 0/255.0, 1.0);
+      vglClCopy(buffer1,vImage);
+
+      vglClDownload(vImage);
+
+      vglImage4To3Channels(vImage);
+
+      fscanf(entrada, "%s", diretorio);
+      cvSaveImage(diretorio, vImage->ipl);
+
+      vglReleaseImage(&tmp);
+      vglReleaseImage(&vImage);*/
+ 
+    VglImage *vImage = vglLoadImage(diretorio);
+    IplImage *tmp = cvCreateImage(cvGetSize(vImage->ipl),vImage->ipl->depth, 1);
+  
+//( (tam - 1) / 2) * (1.0/(5.0*3.0))
+//( (tam - 1) / 2) * (1.0/(5.0*tam*tam))
+//( (tam - 1) / 2) * (1.0/(5.0*tam))
+//( (tam - 1) / 2) * (1.0/(tam*3.0))
+
+  //  float *element = gaussianFunction2D( tam, tam, ( (tam - 1) / 2) * (1.0/(5.0*3.0)) );
+    float *element = gaussianFunction2D( tam, tam, ( (tam - 1) / 2) * (1.0/(5.0*tam)) );
+
+    cvSplit(vImage->ipl, NULL, tmp, NULL, NULL);
+    vglReleaseImage(&vImage);
+
+    //gaussianBlurImage(tmp, tmp, jan, jan, valor1, valor2); 
+   // gaussianBlurImage(tmp, tmp, 7, 7, 1, 1); 
+
+    IplImage *mask = cvCreateImage(cvGetSize(tmp),tmp->depth, tmp->nChannels);
+    cvThreshold(tmp,mask, 20, 255, CV_THRESH_BINARY);
+ //   cvThreshold(tmp,mask, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    aplicarMask(tmp,mask,tmp);
+
+   // gaussianBlurImage(tmp, tmp, jan, jan, valor1, valor2); 
+   // gaussianBlurImage(tmp, tmp, 5, 5, 3, 2); 
+
+    IplImage *imagem = cvCreateImage(cvGetSize(tmp), tmp->depth, 3);
+    cvMerge(tmp,tmp,tmp,NULL, imagem);
+//5x5
+
+    gaussianBlurImage(imagem,imagem, jan1, jan2, val1, val2); 
+   // gaussianBlurImage(imagem,imagem, tam, tam, (tam-1)/2, (tam-1)/2 );
+   // gaussianBlurImage(imagem, imagem, jan, jan, valor1, valor2); 
+    vImage = vglCopyCreateImage( imagem );   
+    vglImage3To4Channels(vImage);
+
+    blackHat( vImage, vImage, element, tam, tam, 1, type);
+//---------------------------
+    vglImage4To3Channels(vImage);
+
+    IplImage *mask2 = cvCreateImage(cvGetSize(imagem),imagem->depth, imagem->nChannels);
+    cvMerge(mask,mask,mask,NULL, mask2);
+    cvCopy(vImage->ipl, imagem);
+    cvErode(mask2,mask2, NULL, 10);
+    aplicarMask(imagem,mask2, imagem);
+
+    vImage = vglCopyCreateImage( imagem );
+//    cvReleaseImage(&imagem);
+/*
+vglImage3To4Channels(vImage);
+vglClDownload(vImage);
+vglImage4To3Channels(vImage);
+cvSaveImage("Arquivos/teste.tif", vImage->ipl);
+*/
+
+
+    vglImage3To4Channels(vImage);
+    VglImage *buffer0 = vglCreateImage(vImage);
+    vglClThreshold(vImage, buffer0, tsy/255.0, 1.0);
+    vglClCopy(buffer0,vImage);
+    vglClDownload(vImage);
+
+
+/*
+    cvSplit(vImage->ipl, NULL, NULL, tmp, NULL);
+    cvThreshold(tmp,tmp, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    cvMerge(tmp,tmp,tmp,NULL, imagem);
+    vImage = vglCopyCreateImage( imagem );
+    
+    vglImage3To4Channels(vImage);
+    VglImage *buffer0 = vglCreateImage(vImage);
+   // vglClDownload(vImage);
+*/
+    VglImage *buffer1 = vglCreateImage(vImage);
+    VglImage *buffer2 = vglCreateImage(vImage);
+    openingByReconstruction( vImage, vImage, buffer1, buffer2, element, tam, tam, 1, type);
+    vglReleaseImage(&buffer1); vglReleaseImage(&buffer2);
+
+  //  vglClFuzzyErode(vImage, buffer0, element, tam, tam, type);
+ //   vglClFuzzyErode(buffer0, vImage, element, tam, tam, type);
+  //  vglClCopy(buffer0,vImage);
+
+
+    vglClThreshold(vImage, buffer0, b/255.0, 1.0);
+    vglClCopy(buffer0,vImage);
+
+  //  vglClFuzzyErode(vImage, buffer0, element, tam, tam, type);
+  //  vglClFuzzyErode(buffer0, vImage, element, tam, tam, type);
+  //  vglClCopy(buffer0,vImage);
+    
+    vglReleaseImage(&buffer0);
+    vglClDownload(vImage);
+
+    vglImage4To3Channels(vImage);
+
+    fscanf(entrada, "%s", diretorio);
+    cvSaveImage(diretorio, vImage->ipl);
+
+    vglReleaseImage(&vImage);
+  }
+}
+
+int main(int argc, char** argv){
+  
+  FILE *entrada = fopen(argv[2], "r");
+  int n;
+  int ident;
+
+  fscanf(entrada,"%d", &n);
+switch ( atoi(argv[1]) ){
+      case 1:
+         printf("\n[ Metodo 1 ] - STANDARD\n");
+	 ident = VGL_CL_FUZZY_STANDARD;
+ 	 break;
+      case 2:
+    	 printf("\n[ Metodo 2 ] - ALGEBRAIC\n");
+	 ident = VGL_CL_FUZZY_ALGEBRAIC;
+    	 break;
+      case 3:
+    	 printf("\n[ Metodo 3 ] - BOUNDED\n");
+	 ident = VGL_CL_FUZZY_BOUNDED;
+    	 break;
+      case 4:
+	 printf("\n[ Metodo 4 ] - DRASTIC\n");
+	 ident = VGL_CL_FUZZY_DRASTIC;
+    	 break;
+      case 5:
+	 printf("\n[ Metodo 5 ] - DUBOIS and PRADE\n");
+	 ident = VGL_CL_FUZZY_DAP;
+    	 break;
+      case 6:
+	 printf("\n[ Metodo 6 ] - HAMACHER\n");
+	 ident = VGL_CL_FUZZY_HAMACHER;
+    	 break;
+      case 7:
+    	 printf("\n[ Metodo 7 ] - GEOMETRIC\n");
+	 ident = VGL_CL_FUZZY_GEOMETRIC;
+    	 break;
+      case 8:
+    	 printf("\n[ Metodo 8 ] - ARITHMETIC\n");
+	 ident = VGL_CL_FUZZY_ARITHMETIC;
+    	 break;
+      case 9:
+	 printf("\n[ Metodo 9 ] - CLASSIC\n");
+	 ident = 0;
+    	 break;
+      default:
+    	 printf("*** [Erro] Metodo nao criado\n");
+    	 exit(1);
+  }
+  printf(" *** Tamanho %d, Black Hat: %d, Ts: %f ***\n", atoi(argv[3]), atoi(argv[4]), atof(argv[5]));
+  metodo(entrada, n, ident, atoi(argv[3]), atoi(argv[4]), atof(argv[5]));
+  printf("Fim do metodo\n");
+
+  fclose(entrada);
+  return 0;
+}
